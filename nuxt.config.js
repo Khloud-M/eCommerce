ca3734696d7b38a7d921d6580c8644877af8990b
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-02-27 20:06:24 +0200

    start

---
+++ nuxt.config.js
@@ -0,0 +1,96 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - eCommerce',
+    title: 'eCommerce',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    `~/assets/main.css`
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    "@nuxtjs/pwa",
+    // https://go.nuxtjs.dev/content
+    "@nuxt/content",
+    "vue-sweetalert2/nuxt/no-css",
+    "nuxt-webfontloader",
+  ],
+  webfontloader: {
+    google: {
+      families: ["DM+Sans:wght@400;500;700&display=swap"],
+    },
+  },
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    defaultAssets: {
+      font: {
+        family: "DM Sans",
+        size: 15,
+      },
+    },
+    treeShake: true,
+    theme: {
+      light: true,
+      themes: {
+        light: {
+          primary: colors.deepPurple.darken1,
+          bg: "#ffffff",
+          surface: "#ffffff",
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+        dark: {
+          primary: colors.deepPurple.darken1,
+          bg: "#0a0514",
+          surface: "#130a24",
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
