ba219e0b1ed8d4782d106735805943b5695e9610
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-02-28 19:52:10 +0200

    id component

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,7 +1,55 @@
 <template>
   <div>
     <Nav />
-    hi
+    <v-container v-if="product">
+      <v-row justify="center">
+        <v-col cols="11" md="7">
+          <h2 class="text-center text-md-h4 font-weight-bold">
+            {{ product.name }}
+          </h2>
+          <div class="text-center mt-2">
+            <v-rating
+              readonly
+              half-increments
+              class="mb-2"
+              color="yellow darken-2"
+              background-color="grey lighten-1"
+              :value="product.ratings"
+              dense
+              size="20"
+            ></v-rating>
+            <v-chip
+              small
+              label
+              outlined
+              class="mr-1"
+              v-for="(t, i) in product.tags"
+              :key="`prod${product.id}-${i}`"
+            >
+              {{ t }}
+            </v-chip>
+          </div>
+
+          <br />
+          <v-img
+            width="100%"
+            class="pointer rounded-lg"
+            height="50vh"
+            :src="product.image"
+          >
+          </v-img>
+          <p class="mt-5 mb-7">{{ product.description }}</p>
+          <v-btn
+            min-height="45"
+            min-width="170"
+            class="text-capitalize"
+            color="primary"
+          >
+            Add To Cart
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
     <Footer />
     <ScrollTop />
   </div>
@@ -10,17 +58,17 @@
 <script>
 export default {
   async created() {
-    const p =  this.products = await this.$content("products")
-      .where({ id: this.$route.params.id })
+    const p = await this.$content("products")
+      .where({ id: parseInt(this.$route.params.id) })
       .limit(1)
       .fetch();
-      this.products = p[0]
+    this.product = p[0];
   },
-  data(){
-    return{
-      products:null
-    }
-  }
+  data() {
+    return {
+      product: null,
+    };
+  },
 };
 </script>
 
